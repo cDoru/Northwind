8607b91afc317fd67266fa169cee0374a818d066
Juan Diego Martinez <lmerrin@gmail.com>
2013-04-08 17:32:25 +0200

    Creación de la clase base de servicio

---
+++ Northwind.ServiceBase/ServiceBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+
+namespace Northwind.ServiceBase
+{
+	/// <summary>
+	/// Clase que representa un servicio web
+	/// </summary>
+	public class ServiceBase<TDto, TRepository> : Service
+		where TDto : class
+		where TRepository : class		
+	{
+	}
+}
