3c0b29259bc04540a96996f6c8b4695ae04a1d83
Juan Diego Martinez <lmerrin@gmail.com>
2013-04-09 18:05:59 +0200

    Creación de las clases base de petición y respuesta

---
+++ Northwind.ServiceBase/ResponseBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface.ServiceModel;
+
+namespace Northwind.ServiceBase
+{
+	/// <summary>
+	/// Clase que representa una respuesta básica
+	/// </summary>
+	/// <typeparam name="TDto">Tipo que contiene la respuesta</typeparam>
+	public abstract class ResponseBase<TDto> : IHasResponseStatus
+	{
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		protected ResponseBase()
+		{
+			ResponseStatus = new ResponseStatus();
+		}
+
+		/// <summary>
+		/// Contenido de la respuesta
+		/// </summary>
+		public TDto Result { get; set; }
+
+		#region Miembros de IHasResponseStatus
+
+		/// <summary>
+		/// Status de la respuesta
+		/// </summary>
+		public ResponseStatus ResponseStatus { get; set; } 		
+
+		#endregion
+	}
+}
---
+++ Northwind.ServiceBase/ResponseCollectionBase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.ServiceInterface.ServiceModel;
+
+namespace Northwind.ServiceBase
+{
+	/// <summary>
+	/// Clase que representa una respuesta básica cuando hay una colección de elementos
+	/// </summary>
+	/// <typeparam name="TDto">Tipo que contiene la respuesta</typeparam>
+	public abstract class ResponseCollectionBase<TDto> : IHasResponseStatus
+		where TDto : class
+	{
+		/// <summary>
+		/// Resultado que contiene la respuesta
+		/// </summary>
+		public IEnumerable<TDto> Result { get; set; }
+
+		/// <summary>
+		/// Constructor de la clase
+		/// </summary>
+		public ResponseCollectionBase()
+		{
+			Result = new List<TDto>();
+		}
+
+		#region Miembros de IHasResponseStatus
+
+		/// <summary>
+		/// Status de la respuesta
+		/// </summary>
+		public ResponseStatus ResponseStatus { get; set; }		
+
+		#endregion
+	}
+}
