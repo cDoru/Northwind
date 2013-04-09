6059dc09a9bed838b4f6fbe7b8e8f7b604c2efae
Juan Diego Martinez <lmerrin@gmail.com>
2013-04-09 18:06:14 +0200

    Creación del esqueleto básico del servicio

---
+++ Northwind.ServiceInterface/Services/CustomersService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+using Northwind.ServiceBase;
+
+namespace Northwind.ServiceInterface.Services
+{
+	/// <summary>
+	/// Servicio de clientes
+	/// </summary>
+	public class CustomersService : Service
+	{
+	}
+}
