103619c99463021381bb57fbffe1769f593e4f7b
Juan Diego Martinez <lmerrin@gmail.com>
2013-04-15 18:03:29 +0200

    Se cambia la herencia de la clase

--- Northwind.ServiceInterface/Services/CustomersService.cs
+++ Northwind.ServiceInterface/Services/CustomersService.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Text;
 using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
+using Northwind.Data.Repositories;
 using Northwind.ServiceBase;
+using Northwind.ServiceModel.Dto;
 
 namespace Northwind.ServiceInterface.Services
 {
 	/// <summary>
 	/// Servicio de clientes
 	/// </summary>
-	public class CustomersService : Service
+	public class CustomersService : ServiceBase<CustomerEntityRepository, Customers>
 	{
 	}
 }
