7ebbd59fd6682a8b39c7e28e50288297912604fb
Juan Diego Martinez <lmerrin@gmail.com>
2013-04-19 13:53:09 +0200

    Se cambian los tipos de la clase

--- Northwind.ServiceInterface/Services/CustomersService.cs
+++ Northwind.ServiceInterface/Services/CustomersService.cs
@@ -2,18 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ServiceStack.Common;
+using ServiceStack.Common.Web;
 using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
+using ServiceStack.Text;
+using Northwind.Data.Model;
 using Northwind.Data.Repositories;
 using Northwind.ServiceBase;
+using Northwind.ServiceModel.Contracts;
 using Northwind.ServiceModel.Dto;
+using Northwind.ServiceModel.Operations;
 
 namespace Northwind.ServiceInterface.Services
 {
 	/// <summary>
 	/// Servicio de clientes
 	/// </summary>
-	public class CustomersService : ServiceBase<CustomerEntityRepository, Customers>
+	public class CustomersService : ServiceBase<ICustomerEntityRepository, CustomerEntity>
 	{
+		public CustomersResponse Get( CustomerIdRequest request )
+		{
+			var result = Repository.Get(request.Id);
+
+			if ( result == null )
+			{
+				throw HttpError.NotFound("Customer {0} not found".Fmt(request.Id));
+			}
+
+			return new CustomersResponse { Result = result.ToDto<Customers>() };
+		}
 	}
 }
