4a11234d55fba5bcd6cb2705ad8087a8e48e12f4
Juan Diego Martinez <lmerrin@gmail.com>
2013-09-16 15:26:32 +0200

    Se añaden comentarios para ApplicationSerializer

--- Northwind.Host.Web/Content/js/app.js
+++ Northwind.Host.Web/Content/js/app.js
@@ -56,15 +56,53 @@ Northwind.ApplicationAdapter = DS.RESTAdapter.extend({
        }
     }
 
+    Ember Data espera un JSON cuyo elemento raíz sea el nombre del modelo en plural. 
+    Por ejemplo, para el caso anterior, Ember Data espera esto: 
+
+    {       
+       customers:[
+          {
+             id:"ALFKI",
+             companyName:"Alfreds Futterkiste",
+             contactName:"Maria Anders",
+             contactTitle:"Sales Representative",
+             address:"Obere Str. 57",
+             city:"Berlin",
+             postalCode:"12209",
+             country:"Germany",
+             phone:"030-0074321",
+             fax:"030-0076545",
+             link:"http://localhost:2828/customers/ALFKI"
+          },
+          {
+             id:"ANATR",
+             companyName:"Ana Trujillo Emparedados y helados",
+             contactName:"Ana Trujillo",
+             contactTitle:"Owner",
+             address:"Avda. de la Constitución 2222",
+             city:"México D.F.",
+             postalCode:"05021",
+             country:"Mexico",
+             phone:"(5) 555-4729",
+             fax:"(5) 555-3745",
+             link:"http://localhost:2828/customers/ANATR"
+          }
+       ]       
+    }
+
 **/
 
 Northwind.ApplicationSerializer = DS.RESTSerializer.extend({
-    // Reestructuramos el nivel superior para organizarlo de la manera que espera Ember    
+    // Reestructuramos el nivel superior para organizarlo de la manera que espera Ember. 
+    // Crearemos un nuevo objeto payload cuyo nivel superior sea el nombre del modelo "primaryType" en plural
     extractArray: function (store, primaryType, payload) {
         var result = payload.result;
         var metadata = payload.metadata;
+
+        // Obtenemos el nombre que tiene que tener el elemento raíz
         var root = Ember.String.pluralize(primaryType.typeKey);
 
+        // Creamos un nuevo objeto como lo quiere Ember Data
         var newPayload = { };
 
         newPayload[root] = result;
@@ -93,18 +131,12 @@ Northwind.Router.map(function () {
 
 Northwind.CustomersRoute = Ember.Route.extend({
     model: function () {
-        //return $.getJSON('http://localhost:2828/customers').then(function(data) {
-        //	return data.result;        
-        //});        
         return this.store.find('customer');
     }
 });
 
 Northwind.CustomerRoute = Ember.Route.extend({
     model: function (params) {
-        //return Ember.$.getJSON('http://localhost:2828/customers/' + params.customer_id).then(function(data) {
-        //    return data.result;
-        //});
         return this.store.find('customer', params.customer_id);
     }
 });
